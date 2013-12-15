884d477c0ee3b2ca751d5d29e0eb5575f83d15cf
Thebookster <thebookster@gmail.com>
2013-12-14 23:55:59 -0500

    15 min update: changed living to bipeds.

--- entities/render/RenderFriendlyGhost.java
+++ entities/render/RenderFriendlyGhost.java
@@ -1,11 +1,11 @@
 package ghostmod.entities.render;
 
 import net.minecraft.client.model.ModelZombie;
-import net.minecraft.client.renderer.entity.RenderLiving;
+import net.minecraft.client.renderer.entity.RenderBiped;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.ResourceLocation;
 
-public class RenderFriendlyGhost extends RenderLiving {
+public class RenderFriendlyGhost extends RenderBiped {
 	
 	public RenderFriendlyGhost()
 	{
--- entities/render/RenderMeanGhost.java
+++ entities/render/RenderMeanGhost.java
@@ -1,11 +1,11 @@
 package ghostmod.entities.render;
 
 import net.minecraft.client.model.ModelZombie;
-import net.minecraft.client.renderer.entity.RenderLiving;
+import net.minecraft.client.renderer.entity.RenderBiped;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.ResourceLocation;
 
-public class RenderMeanGhost extends RenderLiving {
+public class RenderMeanGhost extends RenderBiped {
 
 	public RenderMeanGhost()
 	{
