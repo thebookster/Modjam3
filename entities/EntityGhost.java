8ff131488915f4235effb09029622cf50d17c5a4
Thebookster <thebookster@gmail.com>
2013-12-14 23:24:42 -0500

    inventory is fixed!

--- entities/EntityGhost.java
+++ entities/EntityGhost.java
@@ -1,16 +1,15 @@
 package ghostmod.entities;
 
-import java.util.Random;
-
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
 import net.minecraft.world.World;
 
 public class EntityGhost extends EntityMob{
 	
-	private ItemStack[] equipment = new ItemStack[40];
-    protected float[] equipmentDropChances = new float[40];
+	private ItemStack[] carriedItems = new ItemStack[50];
 
 	public EntityGhost(World par1World) {
 		super(par1World);
@@ -33,10 +32,68 @@ public class EntityGhost extends EntityMob{
 	
 	@Override
 	protected boolean canDespawn() {
-		
-		return false;
+		return this.entityAge > 1200;
 	}
 	
+	@Override
+	protected void dropEquipment(boolean par1, int par2) {
+		super.dropEquipment(par1, par2);
+		for (int j = 0; j < getCarriedItems().length; ++j)
+        {
+            ItemStack itemstack = getCarriedItems()[j];
+            boolean flag1 = true;
+
+            if (itemstack != null) 
+            {
+                this.entityDropItem(itemstack, 0.0F);
+            }
+        }
+	}
+
+	public ItemStack[] getCarriedItems() {
+		return carriedItems;
+	}
+
+	public void setCarriedItems(ItemStack[] carriedItems) {
+		this.carriedItems = carriedItems;
+	}
+	
+	@Override
+	public void writeEntityToNBT(NBTTagCompound par1NBTTagCompound) {
+		super.writeEntityToNBT(par1NBTTagCompound);
+        NBTTagList nbttaglist = new NBTTagList();
+        NBTTagCompound nbttagcompound1;
+
+        for (int i = 0; i < this.carriedItems.length; ++i)
+        {
+            nbttagcompound1 = new NBTTagCompound();
+
+            if (this.carriedItems[i] != null)
+            {
+                this.carriedItems[i].writeToNBT(nbttagcompound1);
+            }
+
+            nbttaglist.appendTag(nbttagcompound1);
+        }
+
+        par1NBTTagCompound.setTag("CarriedItems", nbttaglist);
+		
+	}
+	
+	@Override
+	public void readEntityFromNBT(NBTTagCompound par1NBTTagCompound) {
+		super.readEntityFromNBT(par1NBTTagCompound);
+        NBTTagList nbttaglist;
+        if (par1NBTTagCompound.hasKey("CarriedItems"))
+        {
+            nbttaglist = par1NBTTagCompound.getTagList("CarriedItems");
+
+            for (int i = 0; i < this.carriedItems.length; ++i)
+            {
+                this.carriedItems[i] = ItemStack.loadItemStackFromNBT((NBTTagCompound)nbttaglist.tagAt(i));
+            }
+        }
+	}
 
 	
 	
--- eventhandlers/EventHookContainer.java
+++ eventhandlers/EventHookContainer.java
@@ -1,6 +1,7 @@
 package ghostmod.eventhandlers;
 
 import ghostmod.entities.EntityFriendlyGhost;
+import ghostmod.entities.EntityGhost;
 import ghostmod.entities.EntityMeanGhost;
 
 import java.util.ArrayList;
@@ -9,31 +10,34 @@ import java.util.Random;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.event.ForgeSubscribe;
-import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
+import net.minecraftforge.event.entity.player.AttackEntityEvent;
 
 public class EventHookContainer {
 
 	@ForgeSubscribe
 	public void livingDropsEvent(LivingDropsEvent event) {
 		if (event.entityLiving instanceof EntityPlayer) {
+			EntityGhost ghost; 
 			Random rand = new Random();
 			if (rand.nextInt(2) == 0) {
-				EntityFriendlyGhost ghost = new EntityFriendlyGhost(event.entityLiving.worldObj, event.entityLiving);
-				ghost.setPositionAndUpdate(event.entityLiving.posX, event.entityLiving.posY+3, event.entityLiving.posZ);
-				if (!event.entityLiving.worldObj.isRemote) {
-					event.entityLiving.worldObj.spawnEntityInWorld(ghost);
-					
-				}
+				ghost = new EntityFriendlyGhost(event.entityLiving.worldObj, event.entityLiving);
 			} else {
-				EntityMeanGhost ghost = new EntityMeanGhost(event.entityLiving.worldObj, event.entityLiving);
-				ghost.setPositionAndUpdate(event.entityLiving.posX, event.entityLiving.posY+3, event.entityLiving.posZ);
-				if (!event.entityLiving.worldObj.isRemote) {
-					event.entityLiving.worldObj.spawnEntityInWorld(ghost);
-				}
+				ghost = new EntityMeanGhost(event.entityLiving.worldObj, event.entityLiving);
+			}
+			ghost.setPositionAndUpdate(event.entityLiving.posX, event.entityLiving.posY+3, event.entityLiving.posZ);
+			ArrayList<EntityItem> drops = event.drops;
+			for (int i = 0; i < drops.size(); i++) {
+				ghost.getCarriedItems()[i] = drops.get(i).getEntityItem();
+			}
+			event.drops.clear();
+			if (!event.entityLiving.worldObj.isRemote) {
+				event.entityLiving.worldObj.spawnEntityInWorld(ghost);
+				
 			}
 
 		}
 	}
+	
 	
 }
